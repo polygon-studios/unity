e3836d86b20aab4e4488f4f9db5312606ce4196b
iryanclarke <clarke_93@hotmail.com>
2016-01-08 12:37:42 -0500

    Adding in DB connector script

---
+++ PyjamaJam2/Assets/Scripts/DatabaseConnector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DatabaseConnector : MonoBehaviour {
+
+	void Start () {
+		string url = "http://45.55.90.100/connect";
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+		// check for errors
+		if (www.error == null)
+		{
+			Debug.Log("WWW Ok!: " + www.data);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+}
