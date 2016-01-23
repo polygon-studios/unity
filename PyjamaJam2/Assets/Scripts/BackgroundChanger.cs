99a539ce07e1c16f08a656f6c82bda83aa954f25
iryanclarke <clarke_93@hotmail.com>
2016-01-23 11:29:45 -0500

    Getting night day change in

---
+++ PyjamaJam2/Assets/Scripts/BackgroundChanger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackgroundChanger : MonoBehaviour {
+
+	
+	// Background transition related variables
+	public bool lightUpBG;
+	public float lastAlpha;
+	public GameObject darkBG;
+	public int whichDeathLvl;
+	public int whichNextLvl;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		GameObject[] BGs = GameObject.FindGameObjectsWithTag("NightMap");
+		
+		foreach (GameObject background in BGs) {
+			float alpha = background.GetComponent<Renderer>().material.color.a;
+
+			Color newColor = new Color(1, 1, 1, 0.0f);
+			background.GetComponent<Renderer>().material.color = newColor;
+			Debug.Log ("Trynna change colour");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.B)){
+			StartCoroutine(FadeTo(1.0f, 3.75f));
+		}
+	}
+	
+	
+
+	IEnumerator FadeTo(float aValue, float aTime)
+	{
+		GameObject[] BGs = GameObject.FindGameObjectsWithTag("NightMap");
+		
+		foreach (GameObject background in BGs) {
+			float alpha = background.GetComponent<Renderer>().material.color.a;
+			for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+			{
+				Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha,aValue,t));
+				background.GetComponent<Renderer>().material.color = newColor;
+				yield return null;
+			}
+		}
+	}
+
+}
