d5f7f689fafe4358a1295679e4af1cf1e44a763e
unknown <s.tegan@hotmail.com>
2016-03-07 23:17:07 -0500

    Character selection avoids already selected characters

--- PyjamaJam2/Assets/Scripts/StarterScripts/CharacterSelector.cs
+++ PyjamaJam2/Assets/Scripts/StarterScripts/CharacterSelector.cs
@@ -1,22 +1,56 @@
-﻿using UnityEngine;using System.Collections;public class CharacterSelector : MonoBehaviour {    public StarterGM starterGM;    public int controllerNum;    int currentSelected = 0;    string playerControllerName;    string joystickX = "_JoystickX";    string buttonA = "_A";    bool joystickReset;    // Use this for initialization    void Start () {        playerControllerName = starterGM.playerControllerName;	}		// Update is called once per frame	void Update () {        if (starterGM.passedStartScreen == true)
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelector : MonoBehaviour {
+    public StarterGM starterGM;
+    public int controllerNum;
+    int currentSelected = 0;
+
+    string playerControllerName;
+    string joystickX = "_JoystickX";
+    string buttonA = "_A";
+
+    bool joystickReset;
+    bool characterSelected = false;
+
+    // Use this for initialization
+    void Start () {
+        playerControllerName = starterGM.playerControllerName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (starterGM.passedStartScreen == true && characterSelected == false)
         {
             //move character selector to right
             if (Input.GetAxis(playerControllerName + controllerNum + joystickX) > 0 && joystickReset == true)
             {
-                if (currentSelected == starterGM.possibleCharacters.Count - 1)
-                    currentSelected = 0;
-                else
-                    currentSelected++;
+                bool onUnselectedChar = false;
+                while (onUnselectedChar == false)
+                {
+                    if (currentSelected == starterGM.possibleCharacters.Count - 1)
+                        currentSelected = 0;
+                    else
+                        currentSelected++;
+                    if (starterGM.isSelectedCharacter[currentSelected] == false)
+                        onUnselectedChar = true;
+                }
                 transform.position = new Vector3(starterGM.possibleCharacters[currentSelected].transform.position.x, transform.position.y, 0f);
                 joystickReset = false;
             }
             //move character selector to left
             else if (Input.GetAxis(playerControllerName + controllerNum + joystickX) < 0 && joystickReset == true)
             {
-                if (currentSelected == 0)
-                    currentSelected = 3;
-                else
-                    currentSelected--;
+                bool onUnselectedChar = false;
+                while (onUnselectedChar == false)
+                {
+                    if (currentSelected == 0)
+                        currentSelected = 3;
+                    else
+                        currentSelected--;
+                    if (starterGM.isSelectedCharacter[currentSelected] == false)
+                        onUnselectedChar = true;
+                }
                 transform.position = new Vector3(starterGM.possibleCharacters[currentSelected].transform.position.x, transform.position.y, 0f);
                 joystickReset = false;
             }
@@ -31,4 +65,10 @@
             if (Input.GetButtonDown(playerControllerName + controllerNum + buttonA))
             {
                 starterGM.setCharToController(currentSelected, controllerNum);
-            }        }    }}
+                starterGM.updateScreen(currentSelected);
+                characterSelected = true;
+            }
+        }
+
+    }
+}
