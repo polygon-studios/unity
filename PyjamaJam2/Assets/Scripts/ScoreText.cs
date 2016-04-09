d7259aae8c887bfef21bde6f7cef409ab8da54ba
iryanclarke <clarke_93@hotmail.com>
2016-04-08 22:45:28 -0400

    Fixing some scoretext issue

--- PyjamaJam2/Assets/Scripts/ScoreText.cs
+++ PyjamaJam2/Assets/Scripts/ScoreText.cs
@@ -18,7 +18,7 @@ public class ScoreText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (customText) {
+		if (customText != null) {
 			
 		} else {
 			txt.text = "" + currentScore;
